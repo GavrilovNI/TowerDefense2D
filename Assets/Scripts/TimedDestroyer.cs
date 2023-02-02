59ead5151d2baf565f05795593ce910f07fb6fdd
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-02-02 17:49:17 +0100

    feat: add timed destroyer

---
+++ Assets/Scripts/TimedDestroyer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class TimedDestroyer : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityTimer _timer = new(1, false);
+
+        private void Destroy()
+        {
+            GameObject.Destroy(gameObject);
+        }
+
+        private void Awake()
+        {
+            _timer.Fired += Destroy;
+        }
+
+        private void Update()
+        {
+            _timer.Tick();
+        }
+
+        private void OnDestroy()
+        {
+            _timer.Fired -= Destroy;
+        }
+    }
+}
