2df4c394fbf01622826764d184ff937e3d6f4f01
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-02-02 17:48:48 +0100

    feat: add unity extensions

---
+++ Assets/Scripts/UnityExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+namespace UnityEngine.Extensions
+{
+    public static class UnityExtensions
+    {
+        public static bool IsNull(this UnityEngine.Object obj)
+        {
+            return obj == null || obj.Equals(null);
+        }
+
+        public static bool IsNotNull(this UnityEngine.Object obj)
+        {
+            return IsNull(obj) == false;
+        }
+    }
+}
