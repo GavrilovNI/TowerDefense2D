c9b0065d698471d1bdd1a880dfb039aff1f5bd68
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-02-02 17:52:04 +0100

    feat: add homing bullet

---
+++ Assets/Scripts/HomingBullet.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Extensions;
+
+namespace Game
+{
+    public class HomingBullet : MonoBehaviour
+    {
+        [SerializeField, Min(0)]
+        private float _speed = 1f;
+        [SerializeField, Min(0)]
+        private float _damage = 30f;
+        [SerializeField]
+        private Transform _target;
+
+        public void SetTarget(Transform target)
+        {
+            _target = target;
+        }
+
+        private void FixedUpdate()
+        {
+            bool hasTarget = _target.IsNotNull();
+
+            if (hasTarget)
+                transform.up = (_target.position - transform.position).normalized;
+
+            transform.position += transform.up * Time.fixedDeltaTime * _speed;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            OnHit(collision.transform);
+        }
+
+        private void OnHit(Transform target)
+        {
+            IDamageable damageable = target.GetComponent<IDamageable>();
+            if(damageable != null)
+                damageable.Damage(_damage);
+
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
