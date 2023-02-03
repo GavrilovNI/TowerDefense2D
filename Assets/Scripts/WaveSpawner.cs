1798001aa480fcdff7b10b4f7a6c9bad6e4eb31f
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-02-03 17:21:14 +0100

    feat: create wave spawner

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,48 @@
+﻿using Game.Waves;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Game
+{
+    public class WaveSpawner : MonoBehaviour
+    {
+        public event Action<WaveSpawner> SpawnedAll;
+
+        public bool IsSpawning => _spawnCoroutine != null;
+        public EnemySpawner EnemySpawner => _enemySpawner;
+
+        [SerializeField]
+        private EnemySpawner _enemySpawner;
+        [SerializeReference]
+        private IWave _wave = new SimpleWave();
+
+        private Coroutine _spawnCoroutine;
+
+        [ContextMenu("Start Spawning")]
+        public void StartSpawning()
+        {
+            if(IsSpawning)
+                throw new InvalidOperationException("Another spawning is in progress.");
+
+            _spawnCoroutine = StartCoroutine(Spawn());
+        }
+
+        [ContextMenu("Start Spawning")]
+        public void StopSpawning()
+        {
+            if(IsSpawning == false)
+                throw new InvalidOperationException("Spawning is not in progress");
+
+            StopCoroutine(_spawnCoroutine);
+            _spawnCoroutine = null;
+        }
+
+        private IEnumerator Spawn()
+        {
+            yield return _wave.Spawn(_enemySpawner);
+            _spawnCoroutine = null;
+            SpawnedAll?.Invoke(this);
+        }
+    }
+}
