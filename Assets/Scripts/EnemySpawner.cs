2b8bc13939c101f719826b8639e3a8afc8b7b08a
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-02-03 17:15:06 +0100

    feat: add EnemySpawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        public event Action<Enemy> EnemySpawned;
+
+        [SerializeField]
+        private Path _path;
+
+        public Enemy Spawn(Enemy prefab)
+        {
+            if(_path.Count == 0)
+                throw new InvalidOperationException($"{nameof(_path)} no points.");
+
+            Enemy spawnedEnemy = GameObject.Instantiate(prefab, _path[0], Quaternion.identity, transform);
+
+            PathFollower pathFollower = spawnedEnemy.GetComponent<PathFollower>();
+            if(pathFollower != null)
+                pathFollower.StartPathFromBegin(_path);
+
+            EnemySpawned?.Invoke(spawnedEnemy);
+
+            return spawnedEnemy;
+        }
+    }
+}
