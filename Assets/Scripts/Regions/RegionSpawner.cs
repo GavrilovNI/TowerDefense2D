fe5c09c4391ea1d940a3d1bb8d977e120c63a1ad
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-02-12 17:34:05 +0100

    feat: add RegionSpawner

---
+++ Assets/Scripts/Regions/RegionSpawner.cs
@@ -0,0 +1,30 @@
+﻿using Game.CustomAttributes;
+using Game.EnemyEffects.Effects.Abstract;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Regions
+{
+    public class RegionSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private RegionWithEffects _regionPrefab;
+
+        [SerializeField]
+        [ExtendScriptableObject]
+        protected List<EnemyEffect> OnEnterEffects = new();
+
+        [SerializeField]
+        [ExtendScriptableObject]
+        protected List<EnemyEffect> InsideContinuousEffects = new();
+
+
+        public void Spawn()
+        {
+            RegionWithEffects region = GameObject.Instantiate(_regionPrefab, transform.position, transform.rotation);
+
+            region.AddOneTimeEffects(OnEnterEffects);
+            region.AddContinuousEffects(InsideContinuousEffects);
+        }
+    }
+}
