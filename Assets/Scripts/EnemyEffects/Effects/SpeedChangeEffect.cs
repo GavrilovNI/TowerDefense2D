ef0c524a618833f4a9bd8d07cf20d34abe05d004
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-02-12 17:23:22 +0100

    feat: add speed change effect

---
+++ Assets/Scripts/EnemyEffects/Effects/SpeedChangeEffect.cs
@@ -0,0 +1,39 @@
+﻿using Game.Enemies;
+using Game.EnemyEffects.Effects.Abstract;
+using Game.Paths;
+using UnityEngine;
+
+namespace Game.EnemyEffects.Effects
+{
+    [CreateAssetMenu(fileName = nameof(SpeedChangeEffect), menuName = EnemyEffect.DefaultMenuPath + nameof(SpeedChangeEffect))]
+
+    public class SpeedChangeEffect : StartEndEnemyEffect
+    {
+        [SerializeField, Min(0.001f)]
+        private float _speedChangeRatio;
+
+        public SpeedChangeEffect() : this(1, 1)
+        {
+
+        }
+
+        public SpeedChangeEffect(float durationInSeconds, float speedChangeRatio) : base(durationInSeconds)
+        {
+            _speedChangeRatio = speedChangeRatio;
+        }
+
+        protected override void OnEffectStart(Enemy enemy)
+        {
+            PathFollower pathFollower = enemy.GetComponent<PathFollower>();
+            if(pathFollower != null)
+                pathFollower.Speed *= _speedChangeRatio;
+        }
+
+        protected override void OnEffectEnd(Enemy enemy)
+        {
+            PathFollower pathFollower = enemy.GetComponent<PathFollower>();
+            if(pathFollower != null)
+                pathFollower.Speed /= _speedChangeRatio;
+        }
+    }
+}
