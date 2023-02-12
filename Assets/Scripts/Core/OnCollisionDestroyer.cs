47767443b053c832bea3b7b8fcf9cd843481158f
Nikita Gavrilov <pelmeshka1110@gmail.com>
2023-02-12 17:31:07 +0100

    feat: separate HomingBullet into other classes

---
+++ Assets/Scripts/Core/OnCollisionDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Core
+{
+    public class OnCollisionDestroyer : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Core/TargetFollower.cs
@@ -0,0 +1,33 @@
+﻿using Game.Extensions.Unity;
+using UnityEngine;
+
+namespace Game.Core
+{
+    public class TargetFollower : MonoBehaviour
+    {
+        [SerializeField, Min(0)]
+        private float _speed = 1f;
+        [SerializeField]
+        private Transform? _target;
+
+        public void SetTarget(Transform? target)
+        {
+            _target = target;
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            bool hasTarget = _target.IsNotNull();
+
+            if(hasTarget)
+            {
+                Vector3 directionToTarget = _target.position - transform.position;
+                directionToTarget.z = 0;
+                directionToTarget.Normalize();
+                transform.up = directionToTarget;
+            }
+
+            transform.position += _speed * Time.fixedDeltaTime * transform.up;
+        }
+    }
+}
